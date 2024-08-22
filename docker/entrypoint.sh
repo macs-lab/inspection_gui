1af949f7a7bfe9463fd8f8b2c0c4559a170feb34
Colin Acton <actonc@uw.edu>
2024-08-22 10:44:37 -0700

    separated base and overlay dependencies

---
+++ docker/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Source ROS 2
+source /opt/ros/${ROS_DISTRO}/setup.bash
+source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
+
+# Source the base workspace, if built
+if [ -f /tool_ws/install/setup.bash ]
+then
+    source /tool_ws/install/setup.bash
+fi
+
+# Source the overlay workspace, if built
+if [ -f /overlay_ws/install/setup.bash ]
+then
+    source /overlay_ws/install/setup.bash
+fi
+
+exec "$@"
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker compose --context 'inspection-eoat' up leds
